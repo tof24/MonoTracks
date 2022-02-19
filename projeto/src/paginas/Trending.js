0d7729c327b2d2d3d170285b446ba8e216ce2ba5
Leonardodrt <speed.leo593@gmail.com>
2022-02-19 21:50:38 +0000

    andre corrige o trending ta desalinhado o numero 10

--- projeto/src/paginas/Trending.js
+++ projeto/src/paginas/Trending.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import fetchJsonp from "fetch-jsonp";
+import {Row, Col, Container} from 'reactstrap'
 
 function Trending() {
 
-    const[chart, setChart]=useState([])
-    useEffect(()=>{
-        const fetchhh3 = async() =>{
-            try{
+    const [chart, setChart] = useState([])
+    useEffect(() => {
+        const fetchhh3 = async () => {
+            try {
                 // const res = await fetchJsonp(
                 //     "https://api.deezer.com/chart/0/artists"
                 // )
@@ -15,38 +16,69 @@ function Trending() {
                 // console.log(chart);
                 // setChart(chart.data);
 
-                fetchJsonp('https://api.deezer.com/chart/0/albums&output=jsonp')
-                    .then(function(response) {
-                        return response.json();
+                fetchJsonp('https://api.deezer.com/chart/0/tracks&output=jsonp')
+                    .then(response => response.json())
+                    .then(json => {
+                        console.log(json)
+                        setChart(json.data)
                     })
-                    .then(json => console.log(json))
-                    .catch(function(error) { console.log(error); });
-
+                    .catch(function (error) {
+                        console.log(error);
+                    });
 
 
                 //setIsLoading(false);
-            }catch(error){
+            } catch (error) {
                 console.log(error);
             }
         }
         fetchhh3();
-    },[])
-
-    let imagema=[];
-    let nomeal=[];
-    if(chart.data!==undefined){
-        for(let i=0; i<12; i++){
-
-            console.log(chart);
-        }
-    }
 
 
+    }, []);
 
-    return(
+
+    console.log(chart);
+
+
+    // let imagemc=[];
+    // let nomech=[];
+    // if(chart!==undefined){
+    //     for(let i=0; i<12; i++){
+    //         nomech[i]=chart.data[i].name;
+    //         //imagemc[i]=album.album[i].image[3]["#text"];
+    //         console.log(nomech);
+    //     }
+    // }
+
+
+    return (
+
+
         <div>
-            <h1 className={'titulos'}> Trending </h1>
 
+            <h1 className={'trendingtitulo'}> Trending </h1>
+
+            <Container className={'trending'}>
+
+                <Row>
+                    {chart !== [] && chart.map((musica, i) => (
+
+                        <Col className={'col-4'}>
+
+                            <div>
+                                <div key={i}>
+                                    <div className={'mt-4'}>{[i + 1] + '  '} {musica.title}</div>
+                                    <img src={musica.album.cover} alt={musica.title}/>
+                                </div>
+                            </div>
+
+                        </Col>
+                    ))}
+
+                </Row>
+
+            </Container>
         </div>
     )
 
